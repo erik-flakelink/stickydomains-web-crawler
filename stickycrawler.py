91bafb227d71ca81305f8626c7abdbd1ecdb2252
SturdyBen <116659416+benslife12@users.noreply.github.com>
2023-06-02 13:36:22 -0400

    Parked detection
    Now you can detect parked websites 
    
    my next goal is to store all of these functions in a class

--- stickycrawler.py
+++ stickycrawler.py
@@ -4,12 +4,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
+import requests
+
 
 
 #opens chrome window
 options = webdriver.ChromeOptions()
-options.add_argument("headless")
-options.add_argument("--log-level=3")
+options.add_argument("headless") #makes it so that you don't see the browser pop up
+options.add_argument("--log-level=3") #clears all the clutter 
+options.add_argument('--ignore-certificate-errors') #just in case necessary
+options.add_argument('--allow-running-insecure-content') #just in case necessary
 browser = webdriver.Chrome(chrome_options=options)
 
 
@@ -17,6 +21,7 @@ browser = webdriver.Chrome(chrome_options=options)
 term_dict = ["wawreusaeryaye1.org","2.com","1.com"]
 results = {}
 creation_date = {}
+parked_list = {}
 
 #use ICANN to lookup availability and year of creation
 def ICANN_lookup(term_dict):
@@ -55,10 +60,31 @@ def ICANN_lookup(term_dict):
         else:
             creation_date[term] = "Not created yet"
 
-    for i in results:
-        print(f"{i, results[i]},Created on: {creation_date[i]} \n")
+#checks if a domain is parked
+def parked(term_dict):
 
+
+    for i in term_dict:
+        if results[i] == "Taken":
+            
+            try:
+                response = requests.get(f'https://{i}')
+                if response.status_code == 404:
+                    parked_list[i] = "Parked"
+                else:
+                    parked_list[i] = "Taken"
+            except:
+                parked_list[i] = "Parked"
+        else:    
+            parked_list[i] = "Available"
+
+#prints the information you have crawled 
+def print_all(term_dict,results,creation_date,parked):
+    for i in term_dict:
+        print(f"{i, results[i]},{parked_list[i]},Created on: {creation_date[i]} \n")
 
 #play around down here to find the information you need
 ICANN_lookup(term_dict)
+parked(term_dict)
+print_all(term_dict,results,creation_date,parked)
 
